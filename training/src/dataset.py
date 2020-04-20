ff9ca9006570a55abbfd909041a6233557f20330
zhangxiaobing <zhangxiaobing@192.168.100.193>
2020-04-20 17:58:00 +0800

    获取数据的线程在另一个Session里

--- training/src/dataset.py
+++ training/src/dataset.py
@@ -20,16 +20,24 @@ from dataset_augment import pose_random_scale, pose_rotation, pose_flip, pose_re
 from dataset_prepare import CocoMetadata
 from os.path import join
 from pycocotools.coco import COCO
-import multiprocessing
+from multiprocessing import Process,Queue
+import random
+import numpy as np
 
 BASE = "/root/hdd"
 BASE_PATH = ""
-TRAIN_JSON = "ai_challenger_train.json"
-VALID_JSON = "ai_challenger_valid.json"
+#TRAIN_JSON = "ai_challenger_train.json"
+#VALID_JSON = "ai_challenger_valid.json"
+TRAIN_JSON = "train.json"
+VALID_JSON = "test.json"
+
 
 TRAIN_ANNO = None
 VALID_ANNO = None
 CONFIG = None
+
+train_sequence_data = None
+valid_sequence_data = None
 
 
 def set_config(config):
@@ -80,29 +88,71 @@ def _set_shapes(img, heatmap):
         [CONFIG['input_height'] / CONFIG['scale'], CONFIG['input_width'] / CONFIG['scale'], CONFIG['n_kpoints']])
     return img, heatmap
 
+class SequenceData():
+    def __init__(self, anno, batch_size=32, epoch=10, buffer_size=30, is_train=True):
+        self.anno = anno
+        self.batch_size = batch_size
+        self.epoch = epoch
+        self.is_train = is_train
+        self.datas = anno.getImgIds()
+        self.L = len(self.datas) 
+        self.index = []
+        for epoch_id in range(epoch):
+            epoch_index = random.sample(range(self.L), self.L)
+            self.index.extend(epoch_index)
+        self.L = len(self.index) 
+        self.queue = Queue(maxsize=buffer_size)
+        
+        self.Process_num=CONFIG['multiprocessing_num']
+        for i in range(self.Process_num):
+            print(i,'start')
+            ii = int(self.__len__()/self.Process_num)
+            t = Process(target=self.f,args=(i*ii,(i+1)*ii))
+            t.start()
+    def __len__(self):
+        return self.L - self.batch_size
+    def __getitem__(self, idx):
+        batch_indexs = self.index[idx:(idx+self.batch_size)]
+        batch_datas = [self.datas[k] for k in batch_indexs]
+        imgs,heats = self.data_generation(batch_datas)
+        return imgs,heats
+    
+    def f(self,i_l,i_h):
+        for i in range(i_l,i_h):
+            t = self.__getitem__(i)
+            self.queue.put(t)
+
+    def gen(self):
+        while 1:
+            t = self.queue.get()
+            yield t[0],t[1]
+
+    def data_generation(self, batch_datas):
+        #数据预处理操作
+        imgs = []
+        heats = []
+        for d in batch_datas:
+            img, heat = _parse_function(d, self.is_train)
+            #img, heat = _set_shapes(tf.convert_to_tensor(img), tf.convert_to_tensor(heat))
+            imgs.append(img)
+            heats.append(heat)
+
+        return imgs, heats
 
 def _get_dataset_pipeline(anno, batch_size, epoch, buffer_size, is_train=True):
+    global train_sequence_data, valid_sequence_data
 
-    imgIds = anno.getImgIds()
+    if is_train and train_sequence_data is None:
+        train_sequence_data = SequenceData(anno, batch_size, epoch, buffer_size, is_train)
+        dataset = tf.data.Dataset().batch(1).from_generator(train_sequence_data.gen,
+                                           output_types= (tf.float32,tf.float32))
 
-    dataset = tf.data.Dataset.from_tensor_slices(imgIds)
-
-    dataset.shuffle(buffer_size)
-    dataset = dataset.map(
-        lambda imgId: tuple(
-            tf.py_func(
-                func=_parse_function,
-                inp=[imgId, is_train],
-                Tout=[tf.float32, tf.float32]
-            )
-        ), num_parallel_calls=CONFIG['multiprocessing_num'])
-
-    dataset = dataset.map(_set_shapes, num_parallel_calls=CONFIG['multiprocessing_num'])
-    dataset = dataset.batch(batch_size).repeat(epoch)
-    dataset = dataset.prefetch(100)
+    if not is_train and valid_sequence_data is None:
+        valid_sequence_data = SequenceData(anno, batch_size, epoch, buffer_size, is_train)
+        dataset = tf.data.Dataset().batch(1).from_generator(valid_sequence_data.gen,
+                                           output_types= (tf.float32,tf.float32))
 
     return dataset
-
 
 def get_train_dataset_pipeline(batch_size=32, epoch=10, buffer_size=1):
     global TRAIN_ANNO
